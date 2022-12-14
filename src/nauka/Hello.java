92a34b9954eca99b2e99296d1d45cd4e33327e1e
marekdzi <marekd@stk.net.pl>
2022-12-14 14:43:16 +0100

    sukces

--- src/nauka/Hello.java
+++ src/nauka/Hello.java
@@ -4,7 +4,7 @@ public class Hello {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("w koñcu sukces");
+		System.out.println("w koñcu sukces, ale czy na pewno");
 	}
 
 }

55dff18f4e1a7ca05758c35d6f6653b0172cb540
marekdzi <marekd@stk.net.pl>
2022-12-15 09:36:01 +0100

    test

--- src/nauka/Hello.java
+++ src/nauka/Hello.java
@@ -1,10 +1,19 @@
 package nauka;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Hello {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws FileNotFoundException {
 		// TODO Auto-generated method stub
-		System.out.println("w koñcu sukces, ale czy na pewno");
+		File file = new File ("e:\\aaa");
+		Scanner in = new Scanner(file);
+		System.out.println(in.nextLine());
+		
+		String str = new String("ala ma kota");
+		System.out.println(str.substring(0,5));
 	}
 
 }
